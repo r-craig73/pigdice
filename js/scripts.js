3a5e9729e925c82dcc672dd0a63434729a781b9d
Ron Craig <ron.craig@comcast.net>
2019-03-24 16:55:25 -0700

    add interface logic functions, DRY :hammer:

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // Business Side
-
 function Player (name) {
   this.name = name;
   this.score = 0;
@@ -18,9 +17,7 @@ Player.prototype.rollingDice = function() {
       finalScore = this.score;
       this.score = 0;
       this.roundScore = [];
-      $(".random").hide();
-      $(".hold").hide();
-      $(".new-game").show();
+      gameOver();
       return "You rolled a " + finalRoll + " and your score is greater than or equal to 25 ( " + finalScore + " ).  You win!  Happy dance time!";
     } else {
       return "You rolled a " + rollDice + ".  Your score is " + this.score + ".  Do you want to roll or hold?";
@@ -30,16 +27,28 @@ Player.prototype.rollingDice = function() {
     console.log("Total roll: " + this.score);
     this.score = 0;
     this.roundScore = [];
-    $(".random").hide();
-    $(".hold").hide();
-    $(".new-game").show();
+    gameOver();
     return "Oh, Oh, you rolled a 1.  Your score is " + this.score + " and your turn is over :-(";
    }
 };
 
 Player.prototype.hold = function() {
     this.roundScore.push(this.score);
-    return "Your round score is " + this.roundScore.slice(-1).pop() + " ."; // or last this.roundScore index
+    return "Your round score is " + this.roundScore.slice(-1).pop() + "."; // or last this.roundScore index
+};
+
+// User Interface Logic
+function rollingDiceOptions(){
+  $(".random").show();
+  $(".hold").show();
+  $("form#entry").hide();
+  $("h3").hide();
+};
+
+function gameOver(){
+  $(".random").hide();
+  $(".hold").hide();
+  $(".new-game").show();
 };
 
 // Frontend side
@@ -50,10 +59,7 @@ $(document).ready(function() {
     var player1 = new Player(name);
     $("#playername").text(player1.name);
     console.log(player1.name);
-    $(".random").show();
-    $(".hold").show();
-    $("form#entry").hide();
-    $("h3").hide();
+    rollingDiceOptions();
 
     //roll click
     $("#random").click(function(){
@@ -67,13 +73,8 @@ $(document).ready(function() {
         console.log(player1.roundScore);
     });
     //new game click
-    $(".new-game").click(function(event){
+    $(".new-game").click(function(){
       location.reload();
     });
   });
 });
-    // var firstPlayer = $("button#nameButton").val();
-    // var player1 = new Player (name)
-    // var secondPlayer = $(this).find("input#player-2").val();
-    // var player2 = new Player (firstPlayer).val();
-
